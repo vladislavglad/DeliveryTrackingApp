675915f1a0c31deda8b81803b3c258044239995c
vladislavglad <vladglad.996@gmail.com>
2020-07-23 15:38:55 -0400

    Reporting status of delivery to user.

--- frontend/check-tracking.js
+++ frontend/check-tracking.js
@@ -1,16 +1,34 @@
 const form = document.querySelector("form");
 const textField = document.getElementById("trackingNum");
+const statusReportDiv = document.getElementById("report-delivery-status");
 const API = "http://localhost:3000/lookup/:trackingNum";
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const trackingNum = textField.value;
-    console.log(trackingNum);
+    //console.log(trackingNum);
 
     fetch(API.replace(":trackingNum", trackingNum))
     .then((res) => res.json())
-    .then((jsonData) => console.log(jsonData))
-    .catch((err) => console.log(err));
-
+    .then((res) => {
+        if (res.trackingNum) { // if the server found a valid entry.
+            if (res.isDelivered) {
+                statusReportDiv.innerHTML = "Your Package has been Delivered!";
+                statusReportDiv.style.color = "#00FF7F";
+            } else {
+                statusReportDiv.innerHTML = "Your Package has not been Delivered!";
+                statusReportDiv.style.color = "#FF6347";
+            }
+        } else {
+            statusReportDiv.innerHTML = "Invalid Tracking Number!";
+            statusReportDiv.style.color = "#FF6347";
+        }
+        statusReportDiv.style.display = "block";
+    })
+    .catch((err) => {
+        console.log(err);
+        statusReportDiv.innerHTML = "Something went wrong! Please, try again.";
+        statusReportDiv.style.color = "#FF6347";
+    });
 });
